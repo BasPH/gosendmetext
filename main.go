1360d1d966e062a539799c6cdc1a80023828fb1b
Bas Harenslak <basharenslak@godatadriven.com>
2017-10-18 17:34:13 +0300

    Add CLI flags

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 var (
 	debug = kingpin.Flag("debug", "Debug logging").Short('d').Default("false").Bool()
 
+	address = kingpin.Flag("address", "Address (default 127.0.0.1)").Short('a').Default("127.0.0.1").String()
+	port = kingpin.Flag("port", "Port (default 9999)").Short('p').Default("9999").Int()
+	textfile = kingpin.Flag("textfile", "Textfile containing words (default /usr/share/dict/words)").Short('t').Default("/usr/share/dict/words").ExistingFile()
+	sleep = kingpin.Flag("sleep", "Sleep period (default 0.7s)").Short('s').Default("0.7s").Duration()
+
 	logger *zap.SugaredLogger
 )
 
@@ -20,5 +25,11 @@ func init() {
 func main() {
 	kingpin.Parse()
 
-	logger.Infof("CLI flags:\ndebug: %s", *debug)
+	logger.Infof("Parsed CLI flags",
+		zap.Bool("debug", *debug),
+		zap.String("address", *address),
+		zap.Int("port", *port),
+		zap.String("textfile", *textfile),
+		zap.Duration("sleep", *sleep),
+	)
 }
